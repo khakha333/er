bfd5ff3024674fe8c5b9600b48edfff10dc2d888
khakha333 <sojoon123@gmail.com>
2024-05-25 16:59:40 +0900

    first

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const startBuildingButton = document.querySelector('.button');
+
+    startBuildingButton.addEventListener('click', () => {
+        window.location.href = 'index2.html';
+    });
+});
